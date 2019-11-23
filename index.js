e571b9d74d9f7bc7985fe918be9e1337af8fd1c3
Peter Cho <songtaek94@gmail.com>
2019-11-23 04:33:51 -0500

    added wiki command

--- index.js
+++ index.js
@@ -2,24 +2,41 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const TOKEN = process.env.TOKEN;
+const PREFIX = process.env.PREFIX;
+bot.commands = new Discord.Collection();
+const botCommands = require('./modules/commands');
+const logger = require('./modules/logger');
+
+Object.keys(botCommands).map(key => {
+    bot.commands.set(botCommands[key].name, botCommands[key]);
+});
 
 bot.login(TOKEN);
 
 bot.on('ready', () => {
-    console.info(`Logged in as ${bot.user.tag}!`);
+    logger.info(`Logged in as ${bot.user.tag}!`);
 });
 
-bot.on('message', msg => {
-    if (msg.content === 'ping') {
-        msg.reply('pong');
-        msg.channel.send('pong');
+bot.on('error', error => {
+    logger.error(error);
+})
 
-    } else if (msg.content.startsWith('!kick')) {
-        if (msg.mentions.users.size) {
-            const taggedUser = msg.mentions.users.first();
-            msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
-        } else {
-            msg.reply('Please tag a valid user!');
-        }
+bot.on('message', msg => {
+    if (msg.author.equals(bot.user) || !msg.content.startsWith(PREFIX)) {
+        return;
+    }
+
+    const args = msg.content.substring(PREFIX.length).split(" ");
+    const command = args.shift().toLowerCase();
+    logger.info(`Called command: ${command}, ${args}`);
+
+    if (!bot.commands.has(command)) {
+        return;
+    }
+
+    try {
+        bot.commands.get(command).execute(msg, args);
+    } catch (error) {
+        logger.error(error);
     }
 });
---
+++ modules/commands/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+    Wiki: require('./wiki'),
+};
