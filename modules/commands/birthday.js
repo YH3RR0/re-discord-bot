0dcfa249a7074f40b9da0e26da4d8f7787f96ff2
Peter Cho <songtaek94@gmail.com>
2019-12-17 02:03:22 -0500

    updated birthday command

--- modules/commands/birthday.js
+++ modules/commands/birthday.js
@@ -1,11 +1,22 @@
 const logger = require("../logger");
-const mentionUser = require("../helper/mentionUser");
+const mentionUser = require("../helpers/mentionUser");
 
 module.exports = {
 	name: "birthday",
 	description: "Congratulate someone's birthday",
 	execute(msg, args) {
-        msg.channel.send(`Happy Birthday ${args[0]}!`)
-        .catch (logger.error);
+        if (args.length === 0) {
+            msg.channel.send(`Happy Birthday ${msg.author}!`)
+            .catch (logger.error);
+
+            return;
+        }
+
+        const user = mentionUser(args[0]);
+
+        if (msg.guild.member(user)) {
+            msg.channel.send(`Happy Birthday ${args[0]}!`)
+            .catch (logger.error);
+        }
 	},
 };
